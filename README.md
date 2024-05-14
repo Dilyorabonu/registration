4de289d06ea06a092b0a1b7f456385b764bcc528
dilyorabonu <dilyorabonu@gmail.com>
2024-05-15 03:51:14 +0500

    registration

--- README.md
+++ README.md
@@ -1,1 +1,1 @@
-# js
+# Registration
---
+++ index.js
@@ -0,0 +1,13 @@
+// const form = document.getElementById("form");
+// const data = [];
+
+// form.addEventListener("submit", (e) => {
+//   e.preventDefault();
+//   const newUser = {
+//     name: form.name.valueOf,
+//     phoneNumber: form["phone-number"].value,
+//     email: form.email.value,
+//   };
+
+//   data.push(newUser);
+// });
